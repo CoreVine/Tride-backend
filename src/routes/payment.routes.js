62669fac7dce3312bf3db27165bb8564ed3476fb
SHADOWZXCV <faded.lover.shadows@gmail.com>
2025-07-14 12:48:58 +0300

    Added excel export to subscription on a date range

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,14 +1,28 @@
 const { Router } = require("express");
+const { isAdmin } = require("../middlewares/isAccount.middleware");
 const paymentWebHookController = require("../controllers/webhooks/paymob.controller");
 const paymentController = require("../controllers/payments.controller");
-const { isAdmin } = require("../middlewares/isAccount.middleware");
 const authMiddleware = require("../middlewares/auth.middleware");
+const validate = require("../middlewares/validation.middleware");
+const Yup = require("yup");
 
 const paymobPaymentRouter = Router();
 
-paymobPaymentRouter.post('/webhooks/paymob', paymentWebHookController.handlePaymobWebhook);
+const paymentSchema = {
+    query: Yup.object().shape({
+        from: Yup.date().required(),
+        to: Yup.date().required()
+    })
+};
 
+paymobPaymentRouter.post('/webhooks/paymob', paymentWebHookController.handlePaymobWebhook);
 paymobPaymentRouter.get('/payments', authMiddleware, isAdmin, paymentController.getAllPayments);
 paymobPaymentRouter.get('/payments/:id', authMiddleware, isAdmin, paymentController.getPaymentById);
+
+paymobPaymentRouter.get('/payments/export/all',
+    validate(paymentSchema),
+    authMiddleware,
+    isAdmin,
+    paymentController.exportAllPayments);
 
 module.exports = paymobPaymentRouter;
